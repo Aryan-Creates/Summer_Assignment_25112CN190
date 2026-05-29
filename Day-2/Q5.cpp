ae936ef849afc32ef656fc423db9cc87e2e7ad96
Aryan Gupta <guptaaryan4501@gmail.com>
2026-05-29 14:14:29 +0530

    Add : sum of digits of a number C++.

---
+++ Day-2/Q5.cpp
@@ -0,0 +1,16 @@
+// WAP to find sum of digits of a number.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num, sum = 0;
+    cout<<"Enter a number: ";
+    cin>>num;
+    while(num>0){
+        sum += num%10;
+        num/=10;
+    }
+    cout<<"Sum of digits: "<<sum;
+    return 0;
+}
