d41c8b812a7de57b2ca777325e7ce07cdf354a3a
Aryan Gupta <guptaaryan4501@gmail.com>
2026-05-29 14:16:35 +0530

    add: product of digits of a number C++.

---
+++ Day-2/Q7.cpp
@@ -0,0 +1,24 @@
+// WAP to find product of digits of a number.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num, product = 1;
+    cout<<"Enter a positive number: ";
+    cin>>num;
+    if(num==0){
+        cout<<"Product of digits is: 0";
+    }
+    else if(num<0){
+        cout<<"Please enter a positive number !";
+    }
+    else{
+        while(num>0){
+            product *= num%10;
+            num/=10;
+        }
+        cout<<"Product of digits is: "<<product;
+    }
+    return 0;
+}
