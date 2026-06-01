c49c6dde0d86b5bfef925d154c410fef50a24974
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-01 16:58:37 +0530

    add: check armstrong number C++.

--- Day-4/Q15.cpp
+++ Day-4/Q15.cpp
@@ -1,10 +1,33 @@
 // WAP to Check Armstrong number.
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
     int num, ognum, digit=0, rem, sum=0;
     
+    cout<<"Enter a number: ";
+    cin>>ognum;
+    num = ognum;
+
+    while(num>0){
+        digit++;
+        num = num/10;
+    }
+
+    num = ognum;
+    while(num>0){
+        rem = num%10;
+        sum = sum + (int)round(pow(rem, digit));
+        num = num/10;
+    }
+
+    if(sum == ognum){
+        cout<<ognum<<" is an Armstrong number. ";
+    }
+    else{
+        cout<<ognum<<" is not an Armstrong number. ";
+    }
     return 0;
 }
