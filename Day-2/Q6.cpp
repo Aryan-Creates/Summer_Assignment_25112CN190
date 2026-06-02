785563c9664a2339ffdbef8fb46c3471497d40e1
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-02 15:19:20 +0530

    Modified Code.

--- Day-2/Q6.cpp
+++ Day-2/Q6.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main(){
     int num, rev = 0;
+    
     cout<<"Enter a Positive Number: ";
     cin>>num;
+    
     while(num>0){
         rev = rev*10 + num%10;
         num/=10;
     }
+    
     cout<<"Reversed number is: "<<rev;
     return 0;
 }
