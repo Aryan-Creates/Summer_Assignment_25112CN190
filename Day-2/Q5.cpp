89d99c477b55091067600fdd59f7aa93e694b308
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-02 15:18:34 +0530

    Modified Code.

--- Day-2/Q5.cpp
+++ Day-2/Q5.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main(){
     int num, sum = 0;
+    
     cout<<"Enter a number: ";
     cin>>num;
+    
     while(num>0){
         sum += num%10;
         num/=10;
     }
+    
     cout<<"Sum of digits: "<<sum;
     return 0;
 }
