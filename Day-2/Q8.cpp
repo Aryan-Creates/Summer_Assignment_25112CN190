c78b808cddb6b06a1693181f2cb40ffc57749f74
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-02 15:21:56 +0530

    Modified Code.

--- Day-2/Q8.cpp
+++ Day-2/Q8.cpp
@@ -5,23 +5,29 @@ using namespace std;
 
 int main(){
     int num, temp, rev=0;
+    
     cout<<"Enter a positive number: ";
     cin>>num;
+    
     if(num<0){
         cout<<"Please enter a positive number !";
     }
     else{
         temp=num;
+        
         while(temp>0){
             rev=rev*10 + temp%10;
             temp/=10;
         }
+        
         if(num==rev){
             cout<<"The number "<<num<<" is a palindrome.";
         }
         else{
             cout<<"The number "<<num<<" is not a palindrome.";
         }
+    
     }
+    
     return 0;
 }
