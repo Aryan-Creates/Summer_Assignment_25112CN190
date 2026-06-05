17c2c5431ece8e7c528a3cd74bb5d69c84003bf5
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-05 14:14:14 +0530

    Modified Code.

--- Day-5/Q19.cpp
+++ Day-5/Q19.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main(){
     int num;
+    
     cout<<"Enter a number: ";
     cin>>num;
+    
     if(num<0 || num==0){
         cout<<"Please enter a positive integer !";
     }
+    
     else{
         cout<<"Factors of "<<num<<" are : ";
         for(int i=1; i<=num; i++){
