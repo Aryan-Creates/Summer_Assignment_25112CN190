720898fb3d27fecf3bd049821517014d93da21d8
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-06 00:08:16 +0530

    add: decimal to binary C++.

--- Day-6/Q21.cpp
+++ Day-6/Q21.cpp
@@ -1,1 +1,21 @@
-//WAP
+//WAP to convert decimal number to binary number.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int decimal, binary=0, rem, place=1;
+
+    cout<<"Enter a decimal number: ";
+    cin>>decimal;
+
+    while(decimal>0){
+        rem = decimal%2;
+        binary = binary + rem*place;
+        decimal/=2;
+        place*=10;
+    }
+    
+    cout<<"Binary number: "<<binary;
+    return 0;
+}
