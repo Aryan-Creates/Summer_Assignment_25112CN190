fc401cb575f4fda5f2e60704e7d7fd74c589dda8
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-08 00:03:34 +0530

    add: printing alphabet triangle pattern C++.

---
+++ Day-8/Q31.cpp
@@ -0,0 +1,18 @@
+//WAP to print alphabet triangle pattern.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int rows;
+    cout<<"Enter number of rows: ";
+    cin>>rows;
+
+    for(int i=1; i<=rows; i++){
+        for(char ch='A'; ch<'A'+i; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
