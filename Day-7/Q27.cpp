b534bf33c2890a4588a3ac211dafffa8da6b09db
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-07 11:32:59 +0530

    add: recursive sum of digits C++.

--- Day-7/Q27.cpp
+++ Day-7/Q27.cpp
@@ -1,1 +1,27 @@
 //WAP to Recursive sum of digits.
+
+#include <iostream>
+using namespace std;
+
+int sumOFdigits(int num){
+
+    if(num==0){
+        return 0;
+    }
+    return num%10 + sumOFdigits(num/10);
+}
+
+int main(){
+    int num;
+    
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    if(num<0){
+        cout<<"Please enter a positive number.";
+        return 0;
+    }
+    
+    cout<<"The sum of digits of "<<num<<" is: "<<sumOFdigits(num);
+    return 0;
+}
