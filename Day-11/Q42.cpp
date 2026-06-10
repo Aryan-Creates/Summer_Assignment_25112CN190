fac9084b2471c0ec19a0784c6e1342d2343cdb4d
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-11 00:10:02 +0530

    add: Write function to find max C++.

--- Day-11/Q42.cpp
+++ Day-11/Q42.cpp
@@ -1,1 +1,20 @@
 //WAP to Write function to find maximum.
+
+#include <iostream>
+using namespace std;
+
+int max(int num1, int num2){
+    return (num1 > num2) ? num1 : num2;
+}
+int main(){
+    int num1, num2;
+    cout<<"Enter the numbers: ";
+    cin>>num1>>num2;
+
+    if(num1==num2){
+        cout<<"The numbers are equal";
+        return 0;
+    }
+    cout<<max(num1, num2)<<" is greater.";
+    return 0;
+}
