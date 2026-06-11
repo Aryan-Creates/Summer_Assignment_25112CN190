aae8396a997a5a64280f6812350cb2b963c84852
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-12 00:02:52 +0530

    add: Write function for Fibonacci C++.

---
+++ Day-12/Q47.cpp
@@ -0,0 +1,22 @@
+//WAP to Write function for Fibonacci.
+
+#include <iostream>
+using namespace std;
+
+void fibo(int n){
+    int a=0, b=1, c;
+    for(int i=0; i<n; i++){
+        cout<<a<<" ";
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    
+    fibo(n);
+    return 0;
+}
