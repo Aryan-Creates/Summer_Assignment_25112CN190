20429f027aef662c4166b3db324624d7f1161910
Aryan Gupta <guptaaryan4501@gmail.com>
2026-06-21 06:18:05 +0530

    add: reverse string C++.

---
+++ Day-21/Q82.cpp
@@ -0,0 +1,18 @@
+//WAP to Reverse a string.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    
+    cout<<"Reversed string: ";
+    for(int i = str.length() - 1; i >= 0; i--){
+        cout<<str[i];
+    }
+    
+    return 0;
+}
